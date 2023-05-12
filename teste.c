203abfbb5814934e1e68947755dba76cb0a8be60
Samuel <vaninisamuel.w@gmail.com>
2023-05-12 10:03:10 -0300

    Atualização do sistema de entrada, Senha aprimorada, com caracter.

--- teste.c
+++ teste.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define SENHA_CORRETA "1234"
 
 int main(){
-int senha;
+char senha [20];
 int t=0;
 char tent[2][5] = {"DUAS"
                     ,"UMA"};
@@ -12,28 +15,32 @@ printf("---------------------\n\n");
 printf("\n   Seja bem-vindo!\n");
 printf("\n\n---------------------\n\n");
 
- do{
-      printf("\nDigite sua senha: ");
-      scanf("%d", &senha);
+ do {
+      printf("\n\nDigite sua senha: ");
+      scanf("%s", senha);
 
-    if(senha == 1233123214)
+      t++;
+
+      if (strcmp(senha, SENHA_CORRETA) != 0 && t < 3)
+    
       {
-        printf("\nSenha correta");
-        break;
-      }
-      else
         printf("\nSenha incorreta");
-        if(t<2)
-        {
-            printf("\nVoce tem mais %s tentativas", tent [t]);
-        }
-        t++;
-} while (t<3);
+        
+        printf("\n\nVoce tem mais %s tentativas...", tent [t-1]);
+      }
 
+    if (t == 3 && strcmp(senha, SENHA_CORRETA) != 0) 
+      {
+        printf("\nNumero maximo de tentativas atingido! O caixa sera reinicializado.\n");
+        return 1;
+      }
+        
+    } while (strcmp(senha, SENHA_CORRETA) != 0);
 
-
-
+    printf("\nSenha correta \n\nCaixa Aberto!!!");
 
 
   return 0;
+
+
 }
